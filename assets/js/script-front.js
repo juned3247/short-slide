95f4b679f3dd4d6bd901992506096751c37b0119
juned.khatri31 <juned.khatri31@gmail.com>
2018-09-04 17:06:51 +0530

    Added support for both portrait and landscape photos

--- assets/js/script-front.js
+++ assets/js/script-front.js
@@ -6,6 +6,7 @@ jQuery( document ).ready( function() {
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		autoplay: true,
- 		autoplaySpeed: 2000,
+		autoplaySpeed: 2000,
+		adaptiveHeight: true
 	});
 });
